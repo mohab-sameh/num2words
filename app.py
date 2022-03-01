3ef55589b9b0de5bad20fdc681523854514859d0
Mohab <whitearsenic@outlook.com>
2022-03-02 00:09:34 +0200

    added copy button

--- app.py
+++ app.py
@@ -6,11 +6,18 @@ Created on Tue Mar  1 19:34:24 2022
 """
 import streamlit as st
 from hotfix_decorator import decorator_result
+import pyperclip as pc
 
 
-st.title("Arabic Numbers to Text by Mohab Sameh")
+st.title("Simple Arabic Numbers to Text by Mohab Sameh")
 input_val = st.text_input('Insert number here')
 
 
 if input_val:
+    output_val = decorator_result(int(input_val))
     st.code(decorator_result(int(input_val)))
+
+copy_btn = st.button('Copy Text')
+if copy_btn and input_val:
+    pc.copy(str(output_val))
+    st.success("Text Copied")
